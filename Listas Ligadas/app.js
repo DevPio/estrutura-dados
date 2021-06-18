6aa35cef247a41471e70ec075afd051e18454f59
DevPio <luccaspio890@gmail.com>
2021-06-18 01:01:59 -0300

    add push linkedlist

--- Listas Ligadas/app.js
+++ Listas Ligadas/app.js
@@ -4,7 +4,24 @@ import { defaultEquals } from "./utils";
 class LinkedList {
   constructor(equalsFn = defaultEquals) {
     this.count = 0;
-    this.head = undefined;
+    this.head = null;
     this.equalsFn = equalsFn;
+  }
+
+  push(element) {
+    const node = new Node(element);
+    let current;
+    if (this.head == null) {
+      this.head = node;
+    } else {
+      current = this.head;
+      while (current.next != null) {
+        current = current.next;
+      }
+
+      current.next = node;
+    }
+
+    this.count++;
   }
 }

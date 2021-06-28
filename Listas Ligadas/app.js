c1ea1a32fb893fd214ff9c3b9dc0fd31c26d1d1f
DevPio <luccaspio890@gmail.com>
2021-06-27 21:56:20 -0300

    metodo de remocao

--- Listas Ligadas/app.js
+++ Listas Ligadas/app.js
@@ -1,5 +1,5 @@
-import { Node } from "./models";
-import { defaultEquals } from "./utils";
+import { Node } from "./models/index.js";
+import { defaultEquals } from "./utils/index.js";
 
 class LinkedList {
   constructor(equalsFn = defaultEquals) {
@@ -24,4 +24,51 @@ class LinkedList {
 
     this.count++;
   }
+
+  removeAt(index) {
+    let current;
+
+    if (index >= 0 && index <= this.count) {
+      current = this.head;
+    }
+
+    if (index == 0) {
+      this.head = current.next;
+    } else {
+      let previous;
+
+      for (let ite = 0; ite < index; ite++) {
+        previous = current;
+        current = current.next;
+      }
+
+      previous.next = current.next;
+
+      this.count--;
+      return current.element;
+    }
+
+    return undefined;
+  }
+  get _head() {
+    return this.head;
+  }
 }
+
+const linkList = new LinkedList();
+
+linkList.push(1);
+linkList.push(2);
+linkList.push(3);
+
+let iterable = linkList._head;
+
+console.log(linkList.removeAt(1));
+
+console.log(linkList.head);
+
+// while (iterable.next != null) {
+//   console.log(iterable.next);
+
+//   iterable = iterable.next;
+// }

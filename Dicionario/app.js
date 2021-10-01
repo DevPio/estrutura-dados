efdcc3ae0c58e078e4be475577f0708c8a6dc62a
DevPio <luccaspio890@gmail.com>
2021-10-01 00:00:09 -0300

    change dicionary

--- Dicionario/app.js
+++ Dicionario/app.js
@@ -10,6 +10,17 @@ function defaultToString(item) {
   return item.toString();
 }
 
+class ValuePair {
+  constructor(key, value) {
+    this.key = key;
+    this.value = value;
+  }
+
+  toString() {
+    return `${this.key}-${this.value}`;
+  }
+}
+
 class Dicionary {
   constructor(toStrFn = defaultToString) {
     this.toStrFn = toStrFn;
@@ -19,4 +30,53 @@ class Dicionary {
   hasKey(key) {
     return this.table[this.toStrFn(key)] != null;
   }
+
+  remove(key) {
+    if (this.hasKey(key)) {
+      delete this.table[this.toStrFn(key)];
+      return true;
+    }
+    return false;
+  }
+
+  set(key, value) {
+    if (key != null && value != null) {
+      let newKey = this.toStrFn(key);
+      this.table[newKey] = new ValuePair(key, value);
+      return true;
+    }
+    return false;
+  }
+
+  get(key) {
+    const getKey = this.table[this.toStrFn(key)];
+
+    return getKey == null ? undefined : getKey.value;
+  }
+
+  clear() {
+    this.table = {};
+  }
+
+  size() {}
+
+  isEmpty() {}
+  keys() {}
+  values() {}
+
+  keyValues() {}
+
+  forEach(cb) {
+    let index = 0;
+
+    Object.keys(this.table).forEach((key) => {
+      cb(this.table[key].value, index);
+      index++;
+    });
+  }
 }
+
+let dic = new Dicionary();
+
+dic.set("Luccas", "Dado 1");
+dic.set("Jessyca", "Dado 2");

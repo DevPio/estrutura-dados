0713280dc9c4eb93eb2b66aaf1b4675f522e2bd9
DevPio <luccaspio890@gmail.com>
2021-10-11 01:11:18 -0300

    finalizando o dicionario

--- Dicionario/HashTable.js
+++ Dicionario/HashTable.js
@@ -21,6 +21,17 @@ export class HashTable {
   static hashCode(key) {
     return HashTable.loseloseHashCode(key);
   }
+
+  static djb2HashCode(key) {
+    const tableKey = this.toString(key);
+    let hash = 5381;
+
+    for (let index = 0; index < tableKey.length; index++) {
+      hash = hash * 33 + tableKey.charCodeAt(index);
+    }
+
+    return hash % 1013;
+  }
   put(key, value) {
     if (key != null && value != null) {
       const keyString = HashTable.hashCode(key);
@@ -157,12 +168,159 @@ newHash.put("Aethelwulf", "aethelwulf@email.com");
 
 newHash.put("Sargeras", "Ygritte@email.com");
 
-console.log(newHash.get("Jamie"));
-newHash.remove("Jamie");
-newHash.remove("Sue");
+// console.log(newHash.get("Jamie"));
+// newHash.remove("Jamie");
+// newHash.remove("Sue");
 
-console.log(newHash.get("Jamie"));
+// console.log(newHash.get("Jamie"));
 
-newHash.remove("Jonathan");
-newHash.remove("Aethelwulf");
+// newHash.remove("Jonathan");
+// newHash.remove("Aethelwulf");
 console.log(newHash.get("Jamie"));
+console.log(newHash.table);
+
+class HashTableLinearProbing {
+  constructor(equalFn = defaultToString) {
+    this.equalFn = equalFn;
+    this.table = {};
+  }
+
+  put(key, value) {
+    if (key != null && value != null) {
+      const position = HashTable.hashCode(key);
+
+      if (this.table[position] && this.table[position].key == key) return;
+
+      const searchItem = (position) => {
+        if (!this.table[position]) {
+          this.table[position] = new ValuePair(key, value);
+          return true;
+        } else {
+          return searchItem(position + 1);
+        }
+      };
+
+      return searchItem(position);
+    }
+
+    return false;
+  }
+
+  get(key) {
+    const postion = HashTable.hashCode(key);
+    if (this.table[postion] != null) {
+      if (this.table[postion].key == key) {
+        return this.table[postion].value;
+      } else {
+        let index = postion;
+        while (this.table[index] != null && this.table[index].key != key) {
+          index++;
+        }
+
+        if (this.table[index] != null && this.table[index].key == key) {
+          return this.table[postion].value;
+        }
+
+        return false;
+      }
+    }
+
+    return undefined;
+  }
+
+  verifyRemoveSideEffect(key, removedPosition) {
+    const hash = HashTable.hashCode(key);
+    console.log(hash);
+    let index = hash + 1;
+    while (this.table[index] != null) {
+      const posHash = HashTable.hashCode(this.table[index].key);
+
+      if (posHash <= hash || posHash <= removedPosition) {
+        this.table[removedPosition] = this.table[index];
+        delete this.table[index];
+        removedPosition = index;
+      }
+      index++;
+    }
+  }
+
+  remove(key) {
+    if (key != null) {
+      const position = HashTable.hashCode(key);
+
+      if (this.table[position] != null && this.table[position].key == key) {
+        delete this.table[position];
+        this.verifyRemoveSideEffect(key, position);
+        return true;
+      }
+
+      let index = position + 1;
+
+      const incrementItem = (position) => {
+        if (this.table[position] != null && this.table[position].key == key) {
+          delete this.table[position];
+
+          this.verifyRemoveSideEffect(key, position);
+          return true;
+        }
+        return incrementItem(position + 1);
+      };
+
+      return incrementItem(index);
+    }
+
+    return false;
+  }
+}
+
+const hashProibing = new HashTableLinearProbing();
+
+hashProibing.put("Ygritte", "Ygritte@email.com");
+hashProibing.put("Jonathan", "jonathan@email.com");
+hashProibing.put("Jamie", "jamie@email.com");
+hashProibing.put("Jack", "jack@email.com");
+hashProibing.put("Jasmine", "jasmine@email.com");
+hashProibing.put("Jake", "jake@email.com");
+hashProibing.put("Nathan", "nathan@email.com");
+hashProibing.put("Athelstan", "athelstan@email.com");
+hashProibing.put("Sue", "sue@email.com");
+
+hashProibing.put("Aethelwulf", "aethelwulf@email.com");
+
+hashProibing.put("Sargeras", "Ygritte@email.com");
+
+console.log(hashProibing.remove("Jonathan"));
+
+console.log(hashProibing.table);
+
+const map = new Map();
+
+map.set("Ygritte", "Ygritte@email.com");
+map.set("Jonathan", "jonathan@email.com");
+map.set("Jamie", "jamie@email.com");
+map.set("Jack", "jack@email.com");
+map.set("Jasmine", "jasmine@email.com");
+map.set("Jake", "jake@email.com");
+map.set("Nathan", "nathan@email.com");
+map.set("Athelstan", "athelstan@email.com");
+map.set("Sue", "sue@email.com");
+map.set("Aethelwulf", "aethelwulf@email.com");
+map.set("Sargeras", "Ygritte@email.com");
+
+console.log(map.has("Nathan"));
+console.log(map.size);
+console.log(map.keys());
+console.log(map.values());
+console.log(map.get("Nathan"));
+
+console.log(map.delete("Nathan"));
+
+const weakMap = new WeakMap();
+
+const obj1 = {
+  name: "Nathan",
+};
+
+weakMap.set(obj1, "emailqualquer@email.com");
+
+console.log(weakMap.has(obj1));
